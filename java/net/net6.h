fdc814cfdde5d21484cbdcc0eeaf5b28a0d8962c
Matthew Flanagan <mattimustang@gmail.com>
1999-10-20 07:39:36 +0000

    Initital import.

---
+++ java/net/net6.h
@@ -0,0 +1,36 @@
+/*
+ * $Id$
+ *
+ * net6.h - Common definitions for IPv6 java.net implementations.
+ * Copyright (C) 1999 Matthew Flanagan.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Change Log:
+ *
+ * $Log$
+ * Revision 1.1  1999/10/20 07:39:36  mpf
+ * Initital import.
+ *
+ *
+ */
+
+#ifndef __java_net_net6_h__
+#define __java_net_net6_h__
+
+#define DEBUG		1
+#define AI_SERV		"0"		/* used as 'serv' argument to getaddrinfo() */
+
+#endif
