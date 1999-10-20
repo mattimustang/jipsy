792cd9812b87082ea842bed2d2a45a273cb4d4db
Matthew Flanagan <mattimustang@gmail.com>
1999-10-20 23:11:02 +0000

    Initial Import.

---
+++ test/EchoClient.java
@@ -0,0 +1,41 @@
+/* a simple TCP client that connects to the echo port */
+package test;
+import java.io.*;
+import java.net.*;
+
+public class EchoClient {
+	public static void main(String[] args) throws IOException {
+		Socket echoSocket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String host;
+
+		if (args[0] != null)
+			host = new String(args[0]);
+		else
+			host = new String("arak");
+
+		try {
+			echoSocket = new Socket(host, 7);
+			out = new PrintWriter(echoSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(
+										echoSocket.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.err.println("Couldn't get I/O for the connection to" + host);
+			System.exit(1);
+		}
+
+		BufferedReader stdIn = new BufferedReader(
+									new InputStreamReader(System.in));
+		String userInput;
+
+		while ((userInput = stdIn.readLine()) != null) {
+			out.println(userInput);
+			System.out.println("echo: " + in.readLine());
+		}
+		out.close();
+		in.close();
+		stdIn.close();
+		echoSocket.close();
+	}
+}
