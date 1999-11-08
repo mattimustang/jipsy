ab7cc532b40aad84ecccfa373b45c61196912378
Matthew Flanagan <mattimustang@gmail.com>
1999-11-08 13:27:37 +0000

    - Removed unnecessary debugging code. - Removed unused setSocketFamily() function. - General clean up of code.

--- java/net/util.c
+++ java/net/util.c
@@ -21,6 +21,11 @@
  * Change Log:
  *
  * $Log$
+ * Revision 1.4  1999/11/08 13:27:37  mpf
+ * - Removed unnecessary debugging code.
+ * - Removed unused setSocketFamily() function.
+ * - General clean up of code.
+ *
  * Revision 1.3  1999/11/03 22:30:15  mpf
  * - Updated getSocketFileDescriptor() to handle DatagramSocketImpl too.
  *
@@ -39,7 +44,7 @@
 
 /**
  * get the socket file descriptor from a SocketImpl instance
- * @param env	a pointer to the JNi Environment
+ * @param env	a pointer to the JNI Environment
  * @param obj	a SocketImpl instance
  * @returns		the socket file descriptor or -1 if an error occurred
  */
@@ -54,7 +59,8 @@ int getSocketFileDescriptor(JNIEnv *env, jobject obj)
 	socketImplClass = (*env)->FindClass(env, "java/net/SocketImpl");
 	datagramSocketImplClass = (*env)->FindClass(env, "java/net/DatagramSocketImpl");
 	if ((obj != NULL)
-		&& ((*env)->IsInstanceOf(env, obj, socketImplClass) == JNI_TRUE || ((*env)->IsInstanceOf(env, obj, datagramSocketImplClass) == JNI_TRUE))) {
+		&& (((*env)->IsInstanceOf(env, obj, socketImplClass) == JNI_TRUE)
+			|| ((*env)->IsInstanceOf(env, obj, datagramSocketImplClass) == JNI_TRUE))) {
 
 		class = (*env)->GetObjectClass(env, obj);
 
@@ -94,6 +100,12 @@ void throwException(JNIEnv *env, int type, const char *msg)
 		case EX_SOCKET:
 			exception = (*env)->FindClass(env, "java/net/ConnectException");
 			break;
+		case EX_INT_IO:
+			exception = (*env)->FindClass(env, "java/io/InterruptedIOException");
+			break;
+		case EX_ILLEGAL_ARG:
+			exception = (*env)->FindClass(env, "java/lang/IllegalArgumentException");
+			break;
 		default:
 			exception = 0;
 			break;
@@ -112,73 +124,35 @@ void throwException(JNIEnv *env, int type, const char *msg)
 /**
  * Get the port number out of the socket address structure
  * @param ss	a pointer to the socket address structure
- * @returns		the port number
+ * @returns		the port number or -1 if an error occurred
  */
 int getSockAddrPort(struct sockaddr_storage *ss)
 {
-#ifdef DEBUG
-	printf("NATIVE: getSocketPort(): entering\n");
-#endif
-	switch (ss->__ss_family) {
+	switch (SS_FAMILY(ss)) {
 		case AF_INET: {
 			struct sockaddr_in *sin = (struct sockaddr_in *)&ss;
-#ifdef DEBUG
-	printf("NATIVE: getSocketPort(): returning INET\n");
-#endif
 			return (sin->sin_port);
 		}
 		case  AF_INET6: {
 			struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *)&ss;
-#ifdef DEBUG
-	printf("NATIVE: getSocketPort(): returning INET6\n");
-#endif
 			return (sin6->sin6_port);
 		}
 	}
-#ifdef DEBUG
-	printf("NATIVE: getSocketPort(): returning -1\n");
-#endif
 	return -1;
 }
 
-/* change a sockets family to that specifed in the af arg */
-int setSocketFamily(int *sockfd, int af)
-{
-	struct sockaddr_storage ss;
-	int len;
-#ifdef DEBUG
-	printf("NATIVE: setSocketFamily(): entering af = %d\n", af);
-#endif
-
-	if (getsockname(*sockfd, (struct sockaddr *)&ss, &len) == 0) {
-#ifdef DEBUG
-	printf("NATIVE: setSocketFamily(): sockfd family = %d\n", ss.__ss_family);
-#endif
-	}
-
-	if (af == ss.__ss_family)
-		return 0;
-
-	if (setsockopt(*sockfd, IPPROTO_IPV6, IPV6_ADDRFORM, &af, sizeof(af)) < 0)
-		return -1;
-	if (getsockname(*sockfd, (struct sockaddr *)&ss, &len) == 0) {
-#ifdef DEBUG
-	printf("NATIVE: setSocketFamily(): sockfd new family = %d\n", ss.__ss_family);
-#endif
-	}
-#ifdef DEBUG
-	printf("NATIVE: setSocketFamily(): returning\n");
-#endif
-	return 0;
-}
-
+/**
+ * Get the address family of a socket file descriptor.
+ * @param sockfd	a socket file descriptor
+ * @returns			the address family of the socket or -1 if an error occurred
+ */
 int getSocketFamily(int sockfd)
 {
 	struct sockaddr_storage ss;
 	int len = sizeof(ss);
 
 	if (getsockname(sockfd, (struct sockaddr *)&ss, &len) == 0) {
-		return ss.__ss_family;
+		return SS_FAMILY(&ss);
 	}
 
 	return -1;
